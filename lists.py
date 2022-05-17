f04a930abb1dacc16010c37447231d55e128358e
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-05-17 17:22:34 +0800

    Basic list operation

--- lists.py
+++ lists.py
@@ -1,6 +1,22 @@
 # Basic list operation :
 # - Create a list.
+my_list = ["January", "February", "March"]
+print(my_list)
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
 # - Add an item to the list.
+my_list.append("April")
+print(my_list)
 # - Remove an item from the list.
+my_list.remove("January")
+print(my_list)
 # - Change items in the list.
+my_list[0] = "January"
+my_list[-1] = "May"
+print(my_list)
 # - Access items of the list.
+for i in my_list:
+    print(i)
+
+
