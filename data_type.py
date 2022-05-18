e4b41566e181113728f544eb039e464e64e8ccc9
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-05-18 10:36:19 +0800

    Dictionary Data Type

---
+++ data_type.py
@@ -0,0 +1,33 @@
+# String
+message = "Hello, I'm String"
+
+# Integer
+number_int = 33
+
+# Float
+number_float = 9.99
+
+# Boolean
+valid_test_1 = True
+valid_test_2 = False
+
+# list
+# - String list
+list_of_Strings = ["one", "two", "three"]
+# - numbers list (include int and float)
+list_of_numbers = [1, 2.2, 3.003]
+
+# set
+# - String set
+set_of_string = {"one", "two", "three"}
+# - numbers set (include int and float)
+set_of_numbers = {1, 2.2, 3.003}
+
+# dictionary
+# a pair with key and value
+day_and_unit_dict = {"day": 10, "unit": "hours"}
+
+# Q : Why do we need all these data types?
+# A : Depending on what you try to achieve in the program,
+#     it needs a different data type to achieve exactly that.
+#     Each data types has its own specific purpose.
--- main.py
+++ main.py
@@ -1,48 +1,67 @@
-calculation_to_units = 24
-name_of_unit = "hours"
+hour_unit = 24
+minutes_unit = hour_unit * 60
+seconds_unit = minutes_unit * 60
 
 
-def days_to_units(number_of_days):
-    if number_of_days == 1:
-        return f"{number_of_days} day is " \
-               f"{number_of_days * calculation_to_units} {name_of_unit}"
-    elif number_of_days > 0:
+def days_to_units(number_of_days, conversion_unit):
+    #  number_of_days -> days_and_unit_dictionary["days"]
+    # conversion_unit -> days_and_unit_dictionary["units"]
+    if conversion_unit == "hours":
+        if number_of_days == 1:
+            return f"{number_of_days} day is " \
+                   f"{number_of_days * hour_unit} {conversion_unit}\n"
+        elif number_of_days > 0:
+            return f"{number_of_days} days are " \
+                   f"{number_of_days * hour_unit} {conversion_unit}\n"
+    elif conversion_unit == "minutes":
         return f"{number_of_days} days are " \
-               f"{number_of_days * calculation_to_units} {name_of_unit}"
+               f"{number_of_days * minutes_unit} {conversion_unit}\n"
+    elif conversion_unit == "seconds":
+        return f"{number_of_days} days are " \
+               f"{number_of_days * seconds_unit} {conversion_unit}\n"
+    else:
+        return "Unsupported unit.\n"
 
 
 def validate_and_execute():
     try:
-        user_input_number = int(number_of_days_element)
+        # Accessing items in a dictionary
+        # - Items can be accessed by its key name.
+        # - It can use square brackets or get() method.
+        # -- sample_dict["days] or sample_dict.get("days")
+        user_input_number = int(days_and_unit_dictionary["days"])
         if user_input_number > 0:
-            calculation_value = days_to_units(user_input_number)
+            calculation_value = days_to_units(user_input_number,
+                                              days_and_unit_dictionary["unit"])
             print(calculation_value)
         elif user_input_number == 0:
             print("You entered a 0, please enter a valid positive number.")
         elif user_input_number < 0:
             print("You entered a negative number, no conversion for it.")
     except ValueError:
-        print("Your input is not a valid number. Don't ruin my program.")
+        print("Your input is not valid. Don't ruin my program.")
 
 
-# Lists :
-# 1. To store multiple items in a single variable.
-# 2. A list can contain different data type.
+# Allow user to input :
+# 1. number of days, ex. 22
+# 2. unit to convert to, ex. hours
 user_input = ""
 while user_input != "exit":
     user_input = input(
-        "Hey user, \n"
-        "enter number of days as a comma separate list, \n"
-        "and I wii convert it to hours.\n"
-        "Or text exit to end this program.\n")
-    print(type(user_input.split(",")))
-    print(user_input.split(","))
-    # For Loop :
-    # It's used for iterating over a sequence (like a list).
-    # So we can execute smth for each item in a list.
+        "Hey user, enter number of days and conversion unit.\n"
+        "Ex. 1:seconds, 2:minutes, 3:hours ...etc (days:units)\n")
+    days_and_unit = user_input.split(":")
+    # Ex. if user input "45:hours", it would spilt to "["45", "hours"]".
+    print(days_and_unit)
+    # Dictionary
+    # used to store values in "key:value" pairs.
+    # It's a collection, which doesn't allow duplicate values.
+    # {"days": 20, "unit": "hours"}
 
-    # String (10, 22, 50, 100) -> List [10, 22, 50, 100] by using spilt().
-    # String - spilt() : default separator is any whitespace.
-    # Or it can override separator by using ",".
-    for number_of_days_element in user_input.split(","):
-        validate_and_execute()
+    days_and_unit_dictionary = {"days": days_and_unit[0],
+                                "unit": days_and_unit[1]}
+    print(days_and_unit_dictionary)
+    # if user input is "33: hours", then console will shows :
+    # ['33', 'hours']
+    # {'days': '33', 'unit': 'hours'}
+    validate_and_execute()
