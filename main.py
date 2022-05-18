e34bca6bf6bcfda7d01af66fffcb377f14d35dfb
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-05-18 21:27:11 +0800

    List each compony with respective product count.

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
-from helper import validate_and_execute, user_input_message
+import openpyxl
 
-user_input = ""
-while user_input != "exit":
-    user_input = input(user_input_message)
-    days_and_unit = user_input.split(":")
-    print(days_and_unit)
-    days_and_unit_dictionary = {"days": days_and_unit[0],
-                                "unit": days_and_unit[1]}
-    validate_and_execute(days_and_unit_dictionary)
+# read file
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+# which sheet
+product_list = inv_file["Sheet1"]
+
+# Exercises 1 : How many products we have per supplier?
+# Console : {'AAA Company': 43, 'BBB Company': 17, 'CCC Company': 14}
+
+# ------------------  Start  ------------------
+
+product_per_supplier = {}
+# This empty dict is prepared to put the key:value from the file.
+
+# Go through each and every row in the sheet? By using loop.
+# Execute same logic on each item?
+# So if we use loop, how many times to execute?
+# in this file we need to execute 75 times.
+# print(product_list.max_row)
+
+# range() :
+# - creates a sequence of numbers, starting from 0 by default:
+# - range(75) --> [0, 1, 2, 3 ... 74]
+# - so now we have a valid for loop,
+# - we can iterate over and do smth for each item in this list.
+# -- But in our file, the first row is title we don't want to read it.
+# -- So how to start with second row?
+# -- range(2, max_row) means read from second row, and to the last row.
+# -- range(first_num, before_last_num)
+# -- if range() without setting start, the default is 0.
+# -- and in this case, we need to read the last number?
+# -- product_list.max_row +1
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+
+    if supplier_name in product_per_supplier:
+        # if supplier_name exist.
+        # product_per_supplier[supplier_name] = product_per_supplier[
+        #                                           supplier_name] + 1
+        product_per_supplier[supplier_name] += 1
+    else:
+        # if supplier_name not exist.
+        print("Add a new supplier.")
+        product_per_supplier[supplier_name] = 1
+
+print(product_per_supplier)
