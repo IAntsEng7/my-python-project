5f113c24f0baeaeca4b119759c848eb5b75c25c1
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-05-19 13:47:18 +0800

    Classes and Objects 2

--- main_for_user_and_post.py
+++ main_for_user_and_post.py
@@ -1,5 +1,6 @@
 # import user
 from user import User
+from post import Post
 
 # Usually we have a .py file to manage about users.
 # Create Object
@@ -9,7 +10,8 @@ from user import User
 # from user import User
 tom = User("tt@tt.com", "Tom", "t2t0", "Developer")
 devin = User("dd@dd.com", "Devin", "d4d4", "Manager")
-
+new_post_1 = Post("on a secret mission today", tom.name)
+new_post_2 = Post("Nothing to do today.", devin.name)
 
 # Call function (use method)
 tom.get_user_info()
@@ -20,3 +22,10 @@ devin.get_user_info()
 tom.change_job_title("HR")
 tom.get_user_info()
 # User Tom currently works as HR.You can connect them at tt@tt.com
+
+new_post_1.get_post_info()
+# Post: on a secret mission today.
+#                           -- Written by Tom
+new_post_2.get_post_info()
+# Post: Nothing to do today.
+#                           -- Written by Devin
